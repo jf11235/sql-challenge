86e51c3e60e96b42c8311fdcea8afa7f95b0068d
jf11235 <josh.h.feinberg@gmail.com>
2023-02-19 10:57:37 -0800

    fixed create tables code

--- create_tables.sql
+++ create_tables.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE departments (
 	dept_no VARCHAR(10) NOT NULL PRIMARY KEY,
-	dept_name VARCHAR(30) NOT NULL, 
+	dept_name VARCHAR(30) NOT NULL 
 );
 
 --creating a table for deptartment employees
@@ -12,6 +12,27 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no)
 	REFERENCES departments(dept_no)
 );
+
+--creating a table for titles
+CREATE TABLE titles (
+    title_id VARCHAR (10) NOT NULL PRIMARY KEY,
+    title VARCHAR (10) NOT NULL
+);
+
+--creating a table for employees
+CREATE TABLE employees(
+    emp_no VARCHAR (10) NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR (10) NOT NULL,
+    birth_date VARCHAR(10),
+    first_name VARCHAR(20),
+    last_name VARCHAR (20),
+    sex VARCHAR (1),
+    hire_date DATE,
+    FOREIGN KEY (emp_title_id)
+    REFERENCES titles(title_id)
+);
+
+
 --creating a table for department managers
 CREATE TABLE dept_manager (
     dept_no VARCHAR (10) NOT NULL PRIMARY KEY,
@@ -24,33 +45,16 @@ CREATE TABLE dept_manager (
     REFERENCES dept_emp(emp_no)
 );
 
---creating a table for employees
-CREATE TABLE employees(
-    emp_no, VARCHAR (10) NOT NULL PRIMARY KEY,
-    emp_title_id VARCHAR (10) NOT NULL,
-    birth_date VARCHAR(10),
-    first_name VARCHAR(20),
-    last_name VARCHAR (20),
-    SEX VARCHAR (1),
-    hire_date VARCHAR (10),
-    FOREIGN KEY (emp_title_id)
-    REFERENCES titles(title_id)
-);
+
+
 
 
 --creating a table for salaries
 CREATE TABLE salaries (
     emp_no VARCHAR (10) NOT NULL PRIMARY KEY,
-    salary VARCHAR (10) NOT NULL, 
+    salary MONEY NOT NULL, 
     FOREIGN KEY (emp_no)
     REFERENCES employees(emp_no)
 );
 
 
---creating a table for titles
-CREATE TABLE titles (
-    title_id VARCHAR (10) NOT NULL PRIMARY KEY,
-    title VARCHAR (10) NOT NULL,
-    FOREIGN KEY (title_id),
-    REFERENCES employees(emp_title_id)
-);

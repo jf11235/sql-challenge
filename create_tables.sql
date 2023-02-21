d405478be06a4e3be16c4d518fee4da84b6611f3
jf11235 <josh.h.feinberg@gmail.com>
2023-02-21 14:39:36 -0800

    table creation fixes
    fixing tables

--- create_tables.sql
+++ create_tables.sql
@@ -32,9 +32,9 @@ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR (10) NOT NULL,
 	FOREIGN KEY (dept_no)
-	REFERENCES departments(dept_no),
+    REFERENCES departments(dept_no),
     FOREIGN KEY (emp_no)
-	REFERENCES employees(emp_no),
+    REFERENCES employees(emp_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
